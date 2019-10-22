0cd4a5ee53f0fa0b8690a6a2007fc51026a2890d
Vaughan Webb <vaughanw2012@gmail.com>
2019-10-23 11:33:44 +1300

    codey codey code
    make da task loog

--- Assets/Scripts/TaskLog.cs
+++ Assets/Scripts/TaskLog.cs
@@ -5,7 +5,37 @@ using UnityEngine.Networking;
 
 public class TaskLog : NetworkBehaviour
 {
-    
+    public int numberoftasks = 5;
+
+    public enum TASKS
+    {
+        EATFISH,
+        GREETFISHERMAN,
+        USEPAYPHONE,
+        BUYNEWSPAPER,
+        TIEUPBOAT,
+        DROPTHEPACKAGE,
+    };
+
+
+    [System.Serializable]
+    public struct TaskState
+    {
+        public int id; //enum
+        public bool completed;
+        public TaskState(int id, bool completed)
+        {
+            this.id = id;
+            this.completed = completed;
+        }
+    }
+
+    public List<TaskState> TaskStates = new List<TaskState>();
+    public List<int> ExcludedTasks = new List<int>();
+
+
+
+
     void Start()
     {
         if (!isServer)
@@ -13,5 +43,42 @@ public class TaskLog : NetworkBehaviour
             return;
         }
 
+        for (int i = 0; i < System.Enum.GetValues(typeof(TASKS)).Length - numberoftasks; i++)
+        {
+            bool added = true;
+            while (added == true)
+            {
+                int rand = Random.Range(0, System.Enum.GetValues(typeof(TASKS)).Length);
+                for (int j = 0; j < ExcludedTasks.Count; j++)
+                {
+                    if (rand == ExcludedTasks[j])
+                    {
+                        added = false;
+                    }
+                }
+                if (added == true)
+                {
+                    ExcludedTasks.Add(rand);
+                }
+            }
+
+        }
+
+        for (int i = 0; i < System.Enum.GetValues(typeof(TASKS)).Length; i++)
+        {
+            bool Exculded = false;
+            for (int j = 0; j < ExcludedTasks.Count; j++)
+            {
+                if (ExcludedTasks[j] == i)
+                {
+                    Exculded = true;
+                }
+            }
+            if (Exculded == false)
+            {
+                TaskStates.Add(new TaskState(i, false));
+            }
+        }
     }
 }
+
