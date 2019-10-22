775a065cf94bee8a6cf1b07e7d057077951dda12
David Morris <pixelgrapher@outlook.co.nz>
2019-10-23 08:53:04 +1300

    Added DOTween

---
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using DG.Tweening;
+
+public class PlayerCamera : MonoBehaviour {
+
+    public Transform cameraTarget;
+    public Transform cameraPivot;
+    public GameObject player;
+    [SerializeField] private float lerpSpeed = 12f;
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 a = cameraPivot.position;
+        Vector3 b = cameraTarget.position;
+        Vector3 c = player.transform.position;
+
+        cameraPivot.transform.position = Vector3.Lerp(a, b, lerpSpeed * Time.smoothDeltaTime);
+        //transform.DOLookAt(b, 1f, AxisConstraint.None, Vector3.up);
+	}
+
+    public void Shake()
+    {
+        Vector3 b = cameraTarget.position;
+        //transform.DOShakeRotation(3f, 15f, 7, 90f, true);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerLook.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour {
+
+    [SerializeField] private string mouseXInputName, mouseYInputName;
+    [SerializeField] private float mouseSensitivity;
+    [SerializeField] private Transform playerBody;
+
+    private float xAxisClamp;
+
+    private void Awake()
+    {
+        LockCursor();
+        xAxisClamp = 0;
+    }
+
+	void Start ()
+    {
+		
+	}
+	
+    private void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+	void Update ()
+    {
+        CameraRotation();
+	}
+
+    private void CameraRotation()
+    {
+        float mouseX = Input.GetAxisRaw(mouseXInputName) * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxisRaw(mouseYInputName) * mouseSensitivity * Time.deltaTime;
+
+        xAxisClamp += mouseY;
+
+        if (xAxisClamp > 90f)
+        {
+            xAxisClamp = 90f;
+            mouseY = 0f;
+            ClampXaxisRotationToValue(270f);
+        }
+        else if (xAxisClamp < -90f)
+        {
+            xAxisClamp = -90f;
+            mouseY = 0f;
+            ClampXaxisRotationToValue(90f);
+        }
+
+        //transform.RotateAround(transform.position, -transform.right, mouseY);
+        transform.Rotate(Vector3.left * mouseY);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+    private void ClampXaxisRotationToValue(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+    }
+}
