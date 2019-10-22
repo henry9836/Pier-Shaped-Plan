6707e09333c7f7745804f8bb15bc8fdd10cf33db
henry9836 <henry983615@gmail.com>
2019-10-22 19:07:03 +1300

    camera was fixed to not be on all clients

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,24 @@ public class PlayerController : NetworkBehaviour
 
     public float speed = 100.0f;
     public float maxSpeed = 10.0f;
+
+    public GameObject camPrefab;
+    public Transform camRoot;
+
+    private void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        //Spawn camera
+        GameObject camRefer = Instantiate(camPrefab, transform.position, Quaternion.identity);
+        camRefer.transform.parent = camRoot;
+        camRefer.transform.localPosition = Vector3.zero;
+        camRefer.transform.localRotation = Quaternion.identity;
+    }
+
     // Update is called once per frame
     void Update()
     {
