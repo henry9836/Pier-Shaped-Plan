3d1f56f8dfb4fd25a8f2ef3fc6a1635ff2a01af5
David Morris <pixelgrapher@outlook.co.nz>
2019-10-23 14:26:44 +1300

    Some camera improvements
    Made cam lerp smoother. Cam will now dynamically change distance.

--- Assets/Scripts/Player/PlayerLook.cs
+++ Assets/Scripts/Player/PlayerLook.cs
@@ -7,7 +7,10 @@ public class PlayerLook : MonoBehaviour {
     [SerializeField] private string mouseXInputName, mouseYInputName;
     [SerializeField] private float mouseSensitivity;
     [SerializeField] private Transform YRotRef;
+    [SerializeField] private GameObject camRoot;
 
+    public float verticalRotationValue;
+    public float maxDistance = 6f;
     public float maxVerticalRotation = 60f;
     public float minVerticalRotation = 50f;
     private float xAxisClamp;
@@ -30,6 +33,19 @@ public class PlayerLook : MonoBehaviour {
 
 	void Update ()
     {
+        // Calculate vertical rotation value (0-1 float) where 0 is looking fully up and 1 is looking fully down
+        Vector3 eRot = transform.localRotation.eulerAngles;
+        verticalRotationValue = Mathf.DeltaAngle(eRot.x, 360f - minVerticalRotation) / -(minVerticalRotation + maxVerticalRotation);
+        verticalRotationValue = Mathf.Clamp(verticalRotationValue, 0.1f, 1.0f);
+
+        // Update camera distance
+        if (camRoot != null)
+        {
+            camRoot.transform.localPosition = new Vector3(camRoot.transform.localPosition.x, camRoot.transform.localPosition.y, -maxDistance * Mathf.Pow(verticalRotationValue, 0.5f));
+
+        }
+
+        // Update camera rotation
         CameraRotation();
 	}
 
@@ -38,6 +54,7 @@ public class PlayerLook : MonoBehaviour {
         float mouseX = Input.GetAxisRaw(mouseXInputName) * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxisRaw(mouseYInputName) * mouseSensitivity * Time.deltaTime;
 
+        // Clamp vertical rotation
         xAxisClamp += mouseY;
 
         if (xAxisClamp > minVerticalRotation)
