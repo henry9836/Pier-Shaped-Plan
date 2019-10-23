5ef8c15f69cd72b3e9fcccbca899d5f13052cb46
David Morris <pixelgrapher@outlook.co.nz>
2019-10-24 12:31:58 +1300

    Variable FOV based on pitch
    FOV increases when looking up

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -6,20 +6,23 @@ using DG.Tweening;
 public class PlayerCamera : MonoBehaviour {
 
     public Transform cameraTarget;
-    [SerializeField] private float lerpSpeed = 12f;
+    private GameObject camPivot;
+    private GameObject camRoot;
+    private Camera mainCam;
 
     [SerializeField] private string mouseXInputName = "Mouse X", mouseYInputName = "Mouse Y";
     [SerializeField] private float mouseSensitivity = 180f;
-    [SerializeField] private GameObject camPivot;
-    [SerializeField] private GameObject camRoot;
 
+    [SerializeField] private float lerpSpeed = 12f;
     public AnimationCurve distCurve;
-    public AnimationCurve pitchCurve;
+    public AnimationCurve FOVCurve;
     private float pitchValue, pitchValueAdj;
     private Vector3 pitchLerpTarget;
-    public float maxDistance = 6f;
+    public float maxDistance = 5f;
     public float maxPitchDown = 60f;
     public float maxPitchUp = 50f;
+    public float minFOV = 60f;
+    public float maxFOV = 75f;
     private float xAxisRot;
 
     [SerializeField] LayerMask obstacleLayers;
@@ -28,19 +31,17 @@ public class PlayerCamera : MonoBehaviour {
 
     private void Awake()
     {
-        LockCursor();
-        xAxisRot = 0;
-        camCollision = camRoot.GetComponent<CameraCollision>();
-    }
-
-    private void LockCursor()
-    {
         Cursor.lockState = CursorLockMode.Locked;
+        xAxisRot = 0;
+        camPivot = transform.GetChild(0).gameObject;
+        camRoot = transform.GetChild(0).GetChild(0).gameObject;
+        mainCam = transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<Camera>();
+        camCollision = camRoot.GetComponent<CameraCollision>();
     }
 
     void Update () 
     {
-        // Calculate vertical rotation value (0-1 float) where 0 is looking fully up and 1 is looking fully down
+        // Calculate vertical rotation value (0-1 float) where 0 is pitch up and 1 is pitch down
         Vector3 eRot = camPivot.transform.localRotation.eulerAngles;
         pitchValue = Mathf.DeltaAngle(eRot.x, 270f) / -180f;
         pitchValueAdj = Mathf.DeltaAngle(eRot.x, 360f - maxPitchUp) / -(maxPitchUp + maxPitchDown);
@@ -49,10 +50,12 @@ public class PlayerCamera : MonoBehaviour {
         // Update camera distance
         if (camRoot != null)
         {
-            float zOffset = -maxDistance * distCurve.Evaluate(pitchValue) + camCollision.distOffset;
-            //zOffset = Mathf.Clamp(zOffset, 0.5f, maxDistance);
+            float zOffset = Mathf.Lerp(-1f, -maxDistance, distCurve.Evaluate(pitchValueAdj)) + camCollision.distOffset;
             camRoot.transform.localPosition = new Vector3(camRoot.transform.localPosition.x, camRoot.transform.localPosition.y, zOffset);
         }
+
+        // Update camera FOV
+        mainCam.fieldOfView = Mathf.Lerp(minFOV, maxFOV, FOVCurve.Evaluate(1-pitchValueAdj));
 
         // Update camera rotation
         CameraRotation();
@@ -63,7 +66,7 @@ public class PlayerCamera : MonoBehaviour {
         if (cameraTarget != null)
         {
             Vector3 b = cameraTarget.position;
-
+            // Smooth camera follow target
             transform.position = Vector3.Lerp(transform.position, b, lerpSpeed * Time.smoothDeltaTime);
         }
         
@@ -74,19 +77,19 @@ public class PlayerCamera : MonoBehaviour {
         float mouseX = Input.GetAxisRaw(mouseXInputName) * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxisRaw(mouseYInputName) * mouseSensitivity * Time.deltaTime;
 
-        // Clamp vertical rotation
+        // Clamp and smooth vertical rotation
         xAxisRot += mouseY;
-        //xAxisRot = Mathf.LerpAngle(xAxisRot, xAxisRot + mouseY, Time.deltaTime);
+        xAxisRot = Mathf.LerpAngle(xAxisRot, xAxisRot + mouseY, Time.deltaTime * lerpSpeed);
 
         if (xAxisRot > maxPitchUp)
         {
-            xAxisRot = Mathf.LerpAngle(xAxisRot, maxPitchUp, Time.deltaTime * 12f);
+            xAxisRot = maxPitchUp;
             mouseY = 0f;
             ClampXaxisRotationToValue(360f - maxPitchUp);
         }
         else if (xAxisRot < -maxPitchDown)
         {
-            xAxisRot = Mathf.LerpAngle(xAxisRot, -maxPitchDown, Time.deltaTime * 12f);
+            xAxisRot = -maxPitchDown;
             mouseY = 0f;
             ClampXaxisRotationToValue(maxPitchDown);
         }
@@ -98,7 +101,7 @@ public class PlayerCamera : MonoBehaviour {
     private void ClampXaxisRotationToValue(float value)
     {
         Vector3 eulerRotation = camPivot.transform.eulerAngles;
-        eulerRotation.x = Mathf.Lerp(eulerRotation.x, value, Time.deltaTime * 12f);
+        eulerRotation.x = Mathf.Lerp(eulerRotation.x, value, Time.deltaTime * lerpSpeed);
         camPivot.transform.eulerAngles = eulerRotation;
     }
 
