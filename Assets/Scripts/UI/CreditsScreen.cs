26fafcf062c0e1c73e2279e0ee76144fde62ab70
David Morris <pixelgrapher@outlook.co.nz>
2019-10-25 16:28:58 +1300

    Main Menu screen states set up
    Also added splash screen.

---
+++ Assets/Scripts/UI/CreditsScreen.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class CreditsScreen : MonoBehaviour
+{
+    private CanvasGroup canvas;
+    public float beginDelay = 0.2f;
+    public float beginDelayTimer = 0f;
+    private bool beginStarted = false;
+
+    void Awake()
+    {
+        canvas = GetComponent<CanvasGroup>();
+    }
+
+    void Update()
+    {
+        if (beginStarted)
+        {
+            beginDelayTimer = Mathf.MoveTowards(beginDelayTimer, beginDelay, Time.deltaTime);
+        }
+        else
+        {
+            beginDelayTimer = 0f;
+        }
+
+        if (beginDelayTimer >= beginDelay)
+        {
+            canvas.DOFade(1f, 0.25f);
+            canvas.interactable = true;
+            canvas.blocksRaycasts = true;
+
+            beginStarted = false;
+        }
+    }
+
+    public void Begin()
+    {
+        beginStarted = true;
+    }
+
+    public void End()
+    {
+        canvas.DOFade(0f, 0.25f);
+        canvas.interactable = false;
+        canvas.blocksRaycasts = false;
+
+        beginStarted = false;
+    }
+}
---
+++ Assets/Scripts/UI/OptionsMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class OptionsMenu : MonoBehaviour
+{
+    private CanvasGroup canvas;
+
+    void Awake()
+    {
+        canvas = GetComponent<CanvasGroup>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void Begin()
+    {
+        Invoke("BeginAnimation", 0.25f);
+    }
+
+    public void End()
+    {
+        canvas.DOFade(0f, 0.25f);
+        canvas.interactable = false;
+        canvas.blocksRaycasts = false;
+    }
+
+    private void BeginAnimation()
+    {
+        canvas.DOFade(1f, 0.25f);
+        canvas.interactable = true;
+        canvas.blocksRaycasts = true;
+    }
+}
