3fb464c36e134c052dec1407de5365b593a51ae8
Henry <henry983615@gmail.com>
2019-10-30 08:16:06 +1300

    Gameloop
    Game can detect win conditions, bullets deal damage

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BulletController : NetworkBehaviour
+{
+
+    //Destory the object
+    [Command]
+    void CmdDestroyMe()
+    {
+        Debug.Log("Bullet hit a thing bye bye");
+        NetworkServer.Destroy(gameObject);
+    }
+
+    //When we hit something
+    private void OnTriggerEnter(Collider other)
+    {
+        //If it is a player
+        if (other.tag == "Player")
+        {
+            other.GetComponent<PlayerController>().HitByBullet();
+            CmdDestroyMe();
+        }
+        //If it is a other
+        else
+        {
+            CmdDestroyMe();
+        }
+    }
+}
