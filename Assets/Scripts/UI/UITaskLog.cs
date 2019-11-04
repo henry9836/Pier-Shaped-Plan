29a1f57a4de10011f1d265c124b438745ac8081c
David <pixelgrapher@outlook.co.nz>
2019-11-04 13:13:26 +1300

    Task Log UI

--- Assets/Scripts/UI/UITaskLog.cs
+++ Assets/Scripts/UI/UITaskLog.cs
@@ -20,7 +20,7 @@ public class UITaskLog : NetworkBehaviour
     public GameObject taskItemPrefab;
     public Sprite checkboxOn;
     public Sprite checkboxOff;
-    public int taskCount = 5;
+    private int taskCount = 5;
     public string[] tasks;
 
     private GameObject[] nodes;
@@ -80,37 +80,42 @@ public class UITaskLog : NetworkBehaviour
             CmdGetLog();
 
             // Instantiate task list items
-            //taskCount = taskManager.numberoftasks;
-            taskID = new int[taskCount];
-            taskItem = new GameObject[taskCount];
-            taskItemCheckbox = new Image[taskCount];
-            taskItemText = new Text[taskCount];
+            InitializeTasks();
+        }
+    }
 
-            for (int i = 0; i < taskCount; i++)
+    private void InitializeTasks()
+    {
+        // Find cubes that are encoded with task log data
+        nodes = GameObject.FindGameObjectsWithTag("SERVERINFONODE");
+        taskCount = nodes.Length / 2;
+        Debug.Log("Number of tasks: " + taskCount);
+
+        // Initialize arrays
+        taskID = new int[taskCount];                // Task type assigned to each task number
+        taskItem = new GameObject[taskCount];       // Task item prefab with checkbos and text
+        taskItemCheckbox = new Image[taskCount];    // Checkbox image for the task number
+        taskItemText = new Text[taskCount];         // Text for the task number
+
+        for (int i = 0; i < taskCount; i++)
+        {
+            if (nodes[i].transform.position.x == (int)TheGrandExchange.NODEID.TASKLOG)
             {
-                nodes = GameObject.FindGameObjectsWithTag("SERVERINFONODE");
-
-                if (nodes[i].transform.position.x == (int)TheGrandExchange.NODEID.TASKLOG)
-                {
-                    taskID[i] = decoder.Decode(TheGrandExchange.NODEID.TASKLOG, (int)nodes[i].transform.position.z);
-                }
-
-                taskItem[i] = Instantiate(taskItemPrefab);
-                taskItemCheckbox[i] = taskItem[i].transform.GetChild(0).GetComponent<Image>();
-                taskItemText[i] = taskItem[i].transform.GetChild(1).GetComponent<Text>();
-
-                taskItem[i].transform.parent = taskLog.transform;
-                taskItem[i].transform.localPosition = new Vector3(0.0f, 170f - (i * 64f), 0);
-                taskItem[i].transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-
-                // Initialize task strings
-                // here
-
-
-                Debug.Log(i + " task wtih ID " + taskID[i] + " is " + tasks[taskID[i]]);
-
-                taskItemText[i].text = tasks[taskID[i]];
+                // Find chosen tasks and set them in taskID[]
+                taskID[i] = decoder.Decode(TheGrandExchange.NODEID.TASKLOG, (int)nodes[i].transform.position.z);
             }
+
+            taskItem[i] = Instantiate(taskItemPrefab);
+            taskItemCheckbox[i] = taskItem[i].transform.GetChild(0).GetComponent<Image>();
+            taskItemText[i] = taskItem[i].transform.GetChild(1).GetComponent<Text>();
+
+            taskItem[i].transform.parent = taskLog.transform;
+            taskItem[i].transform.localPosition = new Vector3(0.0f, 170f - (i * 64f), 0);
+            taskItem[i].transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+            // Initialize task strings
+            taskItemText[i].text = tasks[taskID[i]];
+            Debug.Log(i + " task wtih ID " + taskID[i] + " is " + tasks[taskID[i]]);
         }
     }
 
