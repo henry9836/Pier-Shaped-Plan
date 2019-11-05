948fbaabba771e5302a5783222b6ade4fe66327c
Henry <henry983615@gmail.com>
2019-11-05 14:16:00 +1300

    new interaction

--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -1,68 +1,91 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Interaction : MonoBehaviour
+using UnityEngine.Networking;
+public class Interaction : NetworkBehaviour
 {
     public float timeToComplete = 5.0f;
     public float currentCompletion = 0.0f;
-    public bool isDone = false;
-    public int ammountOfSkillchecks = 1;
-    public float maxDistance = 10.0f;
 
-    public List<int> interactor = new List<int>();
-    public List<int> skillchecks = new List<int>();
+    public bool clientDone = false;
+
+    //public int ammountOfSkillchecks = 1;
+    public float maxDistance = 5.0f;
+
+    public int interactorable = 0;
+    //public List<int> skillchecks = new List<int>();
 
     public bool once = false;
 
 
 
+
     void Update()
     {
-        if (isDone == false)
+        if (!isLocalPlayer)
         {
+            return;
+        }
 
-            Vector3 thisPos = this.transform.position;
-            GameObject[] Players = GameObject.FindGameObjectsWithTag("Player");
+        Vector3 playerpos = this.transform.position;
+        GameObject[] interactables = GameObject.FindGameObjectsWithTag("Interactable");
 
-            for (int i = 0; i < Players.Length; i++)
+        interactorable = -9999;
+
+        for (int i = 0; i < interactables.Length; i++)
+        {
+            if (Vector3.Distance(playerpos, interactables[i].transform.position) < maxDistance)
             {
-                if (Vector3.Distance(thisPos, Players[i].transform.position) < maxDistance)
+                if (this.transform.gameObject.GetComponent<PlayerController>().tryingToInteract == true)
                 {
-                    if (Players[i].GetComponent<PlayerController>().tryingToInteract == true)
+                    if (this.transform.gameObject.GetComponent<PlayerController>().amHitman == false)
                     {
-                        if (Players[i].GetComponent<PlayerController>().amHitman == false)
-                        {
-                            interactor.Add(i);
-                        }
+                        interactorable = i;
                     }
                 }
             }
+        }
 
-            if (interactor.Count == 0)
-            {
-                currentCompletion = 0.0f;
-                once = false;
-            }
-            else
-            {
-                if (once == false)
-                {
-                    once = true;
-                    //set up skillcheck times
-                }
-                currentCompletion += Time.deltaTime * interactor.Count;
 
-                //if (skillcheck timer appears do skillcheck)
+
+
+        if (interactorable != -9999)
+        {
+            if (once == false)
+            {
+                once = true;
+                //set up skillcheck times
             }
+
+            currentCompletion += Time.deltaTime;
 
             if (currentCompletion >= timeToComplete)
             {
-                isDone = true;
+                currentCompletion = 5.0f;
             }
-
-            interactor.Clear();
+            //if (skillcheck timer appears do skillcheck)
+        }
+        else
+        {
+            currentCompletion = 0.0f;
+            once = false;
         }
 
+        if (currentCompletion >= timeToComplete)
+        {
+            //true communications
+            TaskLog.TASKS theTask = (TaskLog.TASKS)9999;
+
+            for (int i = 0; i < System.Enum.GetValues(typeof(TaskLog.TASKS)).Length; i++)
+            {
+                if (interactables[interactorable].gameObject.layer == LayerMask.NameToLayer(((TaskLog.TASKS)i).ToString()))
+                {
+                    theTask = (TaskLog.TASKS)i;
+                }
+            }
+
+            GetComponent<TaskLog>().CmdDone(theTask);
+            GetComponent<TaskLog>().CmdCompletedTask(theTask);
+        }
     }
 }
